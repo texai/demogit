a476335fb7ebc243b65ecd7c334534d4271dbd73
Ernesto Anaya <ernestex@gmail.com>
2013-09-27 17:36:29 -0500

    A mi me pidieron hacer lo mismo

--- Validate/NoX.php
+++ Validate/NoX.php
@@ -44,8 +44,15 @@ class My_Validate_NoX extends Zend_Validate_Abstract
 
     }
 
+    public function otraFuncio77(){
+        /*Esta
+es
+otra 
+function
+*/
+    }
+
     public function otraFuncio33(){
         
     }
-
 }
